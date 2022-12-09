6096c1973a561b89793d85f0ceb4b3976acbbd3c
Jorge Solis <jorgesolis1989@gmail.com>
2022-12-09 15:16:04 -0500

    cambios bastantes cambiar contraseña y cambiar datos de usuario

---
+++ static/js/demo/llamarMensajes.js
@@ -0,0 +1,19 @@
+
+/**
+ * Created by jorge on 21/03/16.
+ */
+
+// [ DEMO ] GENERATE RANDOM ALERTS
+	// =================================================================
+
+function llamarMensajes	(llamada, mensaje){
+
+	/// CARGAR MENSAJES DE  REGISTRO USUARIOS
+
+	if(  "exito_usuario".localeCompare(llamada) == 0 ){
+
+			alert(""+mensaje);
+
+	}
+
+}
