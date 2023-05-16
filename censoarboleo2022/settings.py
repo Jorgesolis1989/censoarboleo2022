c9918cf1d252011d3ff0ea08756e24071c7a1f00
YordanStarting <Moncayo0514@gmail.com>
2023-05-15 21:59:24 -0500

    Correcion

--- censoarboleo2022/settings.py
+++ censoarboleo2022/settings.py
@@ -105,7 +105,7 @@ DATABASES = {
     'ENGINE': 'django.db.backends.postgresql_psycopg2',
     'NAME': 'CENSOARBOREO2022',
     'USER': 'postgres',
-    'PASSWORD': 'admin123',
+    'PASSWORD': 'Admin123',
     'HOST': '127.0.0.1',
     'PORT': '5432',
     'CONN_MAX_AGE': 500,
